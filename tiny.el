159c3f74e75970808b83fe4b732f180cb76872a3
Oleh Krehel <ohwoeowho@gmail.com>
2015-03-22 18:34:45 +0100

    tiny.el: Update Copyright

--- tiny.el
+++ tiny.el
@@ -1,30 +1,32 @@
 ;;; tiny.el --- Quickly generate linear ranges in Emacs
 
-;; Copyright (C) 2013  Oleh Krehel
+;; Copyright (C) 2013-2015  Free Software Foundation, Inc.
 
 ;; Author: Oleh Krehel <ohwoeowho@gmail.com>
 ;; URL: https://github.com/abo-abo/tiny
 ;; Version: 0.1
+;; Keywords: convenience
 
-;; This file is free software; you can redistribute it and/or modify
+;; This file is part of GNU Emacs.
+
+;; GNU Emacs is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation; either version 3, or (at your option)
-;; any later version.
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
 
-;; This program is distributed in the hope that it will be useful,
+;; GNU Emacs is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
 
-;; For a full copy of the GNU General Public License
-;; see <http://www.gnu.org/licenses/>.
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>
 
 ;;; Commentary:
 ;;
-;; Setup:
-;; In ~/.emacs:
-;;     (require 'tiny)
-;;     (tiny-setup-default)
+;; To set it up, just bind e.g.:
+;;
+;;     (global-set-key (kbd "C-;") 'tiny-expand)
 ;;
 ;; Usage:
 ;; This extension's main command is `tiny-expand'.
