79ecc9d31c4b471ea0864d4c02b8a6cbc0d6d501
Oleh Krehel <ohwoeowho@gmail.com>
2016-08-11 12:34:27 +0200

    Makefile: Fix "test" target offline

--- Makefile
+++ Makefile
@@ -1,8 +1,13 @@
 emacs ?= emacs
+BEMACS = $(emacs) -batch -l targets/elpa.el
+
 all: test
 
+update:
+	$(emacs) -batch -l targets/install-deps.el
+
 test: clean
-	cask exec emacs -Q -batch -l tiny-test.el -l tiny.el -f ert-run-tests-batch-and-exit
+	$(BEMACS) -l tiny-test.el -l tiny.el -f ert-run-tests-batch-and-exit
 
 compile:
 	$(emacs) -Q -batch -f batch-byte-compile tiny.el
---
+++ targets/install-deps.el
@@ -0,0 +1,28 @@
+(setq package-user-dir
+      (expand-file-name (format ".cask/%s/elpa" emacs-version)))
+(require 'dired)
+(dired-create-directory package-user-dir)
+(message "installing in %s ...\n" package-user-dir)
+(package-initialize)
+(setq package-archives
+      '(("melpa" . "http://melpa.org/packages/")
+        ("gnu" . "http://elpa.gnu.org/packages/")))
+(package-refresh-contents)
+
+(defconst tiny-dev-packages
+  '(undercover))
+
+(dolist (package tiny-dev-packages)
+  (unless (package-installed-p package)
+    (ignore-errors
+      (package-install package))))
+
+(save-window-excursion
+  (package-list-packages t)
+  (condition-case nil
+      (progn
+        (package-menu-mark-upgrades)
+        (package-menu-execute t))
+    (error
+     (message "All packages up to date"))))
+
